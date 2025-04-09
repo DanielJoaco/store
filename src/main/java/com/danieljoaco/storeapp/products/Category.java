7a7fffbe25d75e1515fcff8b8405f673379d5f9b
DanielJoaco <danieljorjuela@gmail.com>
2025-04-08 22:32:37 -0500

    Se agregaron categorias y subcategorias

--- src/main/java/com/danieljoaco/storeapp/products/Category.java
+++ src/main/java/com/danieljoaco/storeapp/products/Category.java
@@ -4,42 +4,24 @@ package com.danieljoaco.storeapp.products;
 
 public abstract class   Category {
     
-    private String id;
     private String name;
     
     public enum Categories {
-        LABIOS, OJOS, PIEL, ACCESORIOS, SKINCARE
+        LIPS, EYES, SKIN, ACCESSORIES, SKINCARE
     }
 
-    public Category(String id, String name) {
-        if (!isValidCategory(name)) {
-            throw new IllegalArgumentException("Categoría inválida: " + name);
+    public Category(String name) {
+        if (Categories.valueOf(name.toUpperCase()).name().equals(name.toUpperCase())
+        ) {
+            throw new IllegalArgumentException("Invalid category : " + name);
         }
-        this.id = id;
         this.name = name.toUpperCase();
-    }
-
-    private boolean isValidCategory(String name) {
-        try {
-            Categories.valueOf(name.toUpperCase());
-            return true;
-        } catch (IllegalArgumentException e) {
-            return false;
-        }
-    }
-    
-    public String getid(){
-        return this.id;
     }
 
     public String getName(){
         return this.name;
     }
-    
-    public void setId(String id){
-        this.id = id;
-    }
-    
+
     public void setName(String name){
         this.name = name;
     }
--- src/main/java/com/danieljoaco/storeapp/products/SubCategory.java
+++ src/main/java/com/danieljoaco/storeapp/products/SubCategory.java
@@ -1,8 +1,51 @@
 package com.danieljoaco.storeapp.products;
 
 public class SubCategory extends Category {
-    
-    public SubCategory(String id, String name){
-        super(id, name);
+
+    private String name;
+
+    public enum SubCategories {
+        LIPS("LIPSTICKS", "DELINERS", "SOFT_LIPS", "TINTE", "GLOSS"),
+        EYES("DELINERS", "EYELINER", "EYELASH", "EYE_SHADOW"),
+        SKIN("FOUNDATIONS", "CONCEALERS", "SETTERS", "BLUSHES", "POWDERS");
+
+        private final String[] items;
+
+        SubCategories(String... items) {
+            this.items = items;
+        }
+
+        public String[] getItems() {
+            return items;
+        }
+
+    private static boolean isValidSubCategory(String category, String subcategory) {
+
+            try {
+                SubCategories subCategoryEnum = SubCategories.valueOf(category.toUpperCase());
+                for (String item : subCategoryEnum.getItems()) {
+                    if (item.equalsIgnoreCase(subcategory)) {
+                        return true;
+                    }
+                }
+            } catch (IllegalArgumentException e) {
+                return false;
+            }
+            return false;
+        }
+    }
+
+    public SubCategory(String category, String subcategory) {
+        super(category);
+
+        if (!SubCategories.isValidSubCategory(category, subcategory)) {
+            throw new IllegalArgumentException("The subcategory '" + subcategory + "' isn´t for the category '" + category + "'.");
+        }
+
+        this.name = subcategory.toUpperCase();
+    }
+
+    public String getName() {
+        return name;
     }
 }
